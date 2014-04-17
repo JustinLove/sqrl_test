3bc8607d3e5779425483411f44f7f70af24ccfdc
Justin Love <git@JustinLove.name>
2014-04-17 15:31:26 -0500

    report more information, use reversible nut

--- lib/sqrl/test/server.rb
+++ lib/sqrl/test/server.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
 require 'rqrcode'
-require 'sqrl/opaque_nut'
+require 'sqrl/reversible_nut'
 require 'sqrl/url'
 require 'sqrl/login_request'
 
@@ -20,8 +20,8 @@ module SQRL
       end
 
       get '/' do
-        nut = SQRL::OpaqueNut.new.to_s
-        auth_url = SQRL::URL.new(request.host+'/sqrl', nut).to_s
+        nut = SQRL::ReversibleNut.new(ENV['SERVER_KEY'], request.ip).to_s
+        auth_url = SQRL::URL.new(request.host+':'+request.port.to_s+'/sqrl', nut).to_s
         erb :index, :locals => {
           :auth_url => auth_url,
           :qr => RQRCode::QRCode.new(auth_url, :size => 10)
@@ -29,8 +29,14 @@ module SQRL
       end
 
       post '/sqrl' do
+        nut = SQRL::ReversibleNut.reverse(ENV['SERVER_KEY'], params[:nut])
         req = SQRL::LoginRequest.new(request.body.read)
         props = Hash[RequestProperties.map {|prop| [prop, req.__send__(prop)]}]
+        props['secure?'] = request.secure?
+        props['post ip'] = request.ip
+        props['nut'] = params[:nut]
+        props['login ip'] = nut.ip
+        props['age'] = nut.age
         erb :report, :locals => {
           :req => req,
           :props => props
